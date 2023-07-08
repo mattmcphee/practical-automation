7f298b513a3c20b1381601bbec80a567f5de4f50
mattmcphee <matt.imcphee@gmail.com>
2023-07-08 11:17:33 +1000

    initial commit

---
+++ New-Archive.ps1
@@ -0,0 +1,20 @@
+function Set-ArchiveFilePath {
+    <#
+    .SYNOPSIS
+    Creates an archive.
+    
+    .DESCRIPTION
+    Creates an archive with a timestamp filename.
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        
+    )
+}
